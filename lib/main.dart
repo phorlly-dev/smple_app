3bc61fbf7d7b316756833da0b4611dce628ff337
Phorlly <phorllylan@gmail.com>
2025-05-06 18:24:17 +0700

    updated google service

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
 import 'package:smple_app/views/pages/home.dart';
 
 //global object for accessing device screen size
@@ -11,15 +11,10 @@ Future<void> main() async {
   await Firebase.initializeApp();
   runApp(const MyApp());
 }
- 
-class MyApp extends StatefulWidget {
+
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
